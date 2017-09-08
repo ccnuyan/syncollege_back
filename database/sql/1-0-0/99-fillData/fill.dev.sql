f04b98ede61359183a7d1ca0a159250dab471e01
Zhonghua Yan <ccnuyan@live.com>
2017-09-08 20:09:05 +0800

    use email

--- database/sql/1-0-0/99-fillData/fill.dev.sql
+++ database/sql/1-0-0/99-fillData/fill.dev.sql
@@ -1,4 +1,4 @@
 set search_path = membership;
 -- http://www.w3school.com.cn/sql/index.asp
 
-select * from register('ccnuyan', 'password');
+select * from register('ccnuyan@gmail.com', 'password');
