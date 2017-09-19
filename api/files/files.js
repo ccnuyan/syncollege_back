304898b2fe0202ac24fb48a188177cdd18d0976e
Zhonghua Yan <ccnuyan@live.com>
2017-09-19 20:26:43 +0800

    no message

--- api/files/files.js
+++ api/files/files.js
@@ -79,7 +79,7 @@ const update_file_status = async (req, res) => {
   };
   try {
     const ret = await fileServices.update_file_status({
-      uploader_id: req.user.id,
+      // uploader_id: req.user.id,
       file_id: req.body.id,
       ...query,
     }, req.context);
